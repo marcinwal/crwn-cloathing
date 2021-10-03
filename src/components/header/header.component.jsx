4d16319f5944c784be02df6be0899d8057c55e3c
marcin.walendzik@gmail.com <marcin.walendzik@gmail.com>
2021-10-03 09:02:00 +0200

    using selectors in other components for hidden, user and cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -34,14 +37,25 @@ const Header = ({ currentUser, hidden }) => (
         {hidden ? null : <CartDropdown />}
     </div>
 );
-
+//v1
 // const mapStateToProps = state => ({
 //     currentUser: state.user.currentUser
 // })
-const mapStateToProps = ({user: { currentUser },cart: {hidden }}) => ({
-    currentUser,
-    hidden
-})
+//v2
+// const mapStateToProps = ({user: { currentUser },cart: {hidden }}) => ({
+//     currentUser,
+//     hidden
+// })
+//v3
+// const mapStateToProps = (state) => ({
+//     currentUser: selectCurrentUser(state),
+//     hidden: selectCartHidden(state)
+// });
 
+//v4
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
+});
 
 export default connect(mapStateToProps)(Header);
