f2138133f83dedd7220af18da2dc837f4d0a26ea
marcin.walendzik@gmail.com <marcin.walendzik@gmail.com>
2021-10-04 21:22:16 +0200

    adding redux-persist for localStorage and updating the project to use it'

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducers';
 
-export default combineReducers({
-    user: userReducer,
-    cart: cartReducer
-})
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'] //user is stored with auth; 'cart' is the reducer we want to save!
+};
+
+const rootReducer = combineReducers({
+    user:   userReducer,
+    cart:   cartReducer
+});
+
+export default persistReducer(persistConfig, rootReducer);
