ff9b9c10696a196ba8754c8bc6faef8fe455274f
marcin.walendzik@gmail.com <marcin.walendzik@gmail.com>
2021-10-05 20:33:00 +0200

    collection page; redirect from directory to right pages

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,16 +7,25 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => { 
+    
+    const { title, items } = collection;
+
+    return (
     <div className='collection-page'>
-        <h2>COLLECTION PAGE</h2>
+        <h2 className='title'>{ title }</h2>
+        <div className='items'> 
+        {
+            items.map(item => (<CollectionItem key={item.id} item={item} />
+        ))}
+        </div>
     </div>
-);
+)};
 
 
 //ownProps are own props of the component which are coming including match 
 const mapStateToProps = (state, ownProps) => ({
-    collection: selectCollection(ownProps.match.params.collectionId)(state)
+    collection: selectCollection(ownProps.match.params.collectionId)(state) //TODO UNDERSTAND!!!!
 });
 
 export default connect(mapStateToProps)(CollectionPage);
