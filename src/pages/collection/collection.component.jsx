cfded985bf5c00c92b473c375a9ee513692909ac
marcin.walendzik@gmail.com <marcin.walendzik@gmail.com>
2021-10-05 20:04:08 +0200

    collection page with tricky selectors and complex routing

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => (
+    <div className='collection-page'>
+        <h2>COLLECTION PAGE</h2>
+    </div>
+);
+
+
+//ownProps are own props of the component which are coming including match 
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
