84c7e46c941c46e0d47ab3cfe0b7c88fbb269dcf
marcin.walendzik@gmail.com <marcin.walendzik@gmail.com>
2021-10-17 20:31:05 +0200

    user sagas added but do not work .. sign with google

--- src/App.js
+++ src/App.js
@@ -26,20 +26,20 @@ class App extends React.Component {
 
     console.log('App did mount');
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => { 
-      if (userAuth) {
+    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => { 
+    //   if (userAuth) {
 
-        const userRef = await createUserProfileDocument(userAuth);
+    //     const userRef = await createUserProfileDocument(userAuth);
         
-        userRef.onSnapshot(snapShot => {   //subscription for any changes  !!!
-         setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-          });
-        });
-      }
-      setCurrentUser(userAuth);
-    });
+    //     userRef.onSnapshot(snapShot => {   //subscription for any changes  !!!
+    //      setCurrentUser({
+    //           id: snapShot.id,
+    //           ...snapShot.data()
+    //       });
+    //     });
+    //   }
+    //   setCurrentUser(userAuth);
+    // });
   }
 
   componentWillUnmount() {
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth} from '../../firebase/firebase.utils';
+import { googleSignInStart } from '../../redux/user/user.actions';
 
 import './sign-in.styles.scss';
 
@@ -39,6 +41,7 @@ class SignIn extends React.Component {
     }
 
     render() {
+        const { googleSignInStart} = this.props;
         return(
             <div className='sign-in'>
                 <h2>I already have an account</h2>
@@ -61,13 +64,22 @@ class SignIn extends React.Component {
                     />
                     <div className='buttons'>
                         <CustomButton type='submit'>Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>                   
+                        <CustomButton 
+                            type='button' 
+                            onClick={googleSignInStart} 
+                            isGoogleSignIn
+                        >
+                            Sign In with Google
+                        </CustomButton>                   
                     </div>
-
                 </form>
             </div>
         )
     }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart())
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
