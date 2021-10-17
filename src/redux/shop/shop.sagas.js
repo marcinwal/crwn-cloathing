fb0b20938a06e7b0113f0fca50febbf9a50dad9c
marcin.walendzik@gmail.com <marcin.walendzik@gmail.com>
2021-10-17 16:25:36 +0200

    rootSaga file created

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,12 +1,34 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, takeLatest, call, put } from 'redux-saga/effects';
+
+import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
+
+import {
+    fetchCollectionsSuccess,
+    fetchCollectionsFailure
+} from './shop.actions';
 
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
     yield console.log('I am fired');
+    
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionMap = yield call(convertCollectionSnapshotToMap, snapshot); 
+        yield put(fetchCollectionsSuccess(collectionMap)); //put it DISPATCH in SAGA
+    } catch(error){
+        yield put(fetchCollectionsFailure(error.message));
+    }
+        // collectionRef
+        // .get()
+        // .then(snapshot => {
+        //     const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+        //     dispatch(fetchCollectionsSuccess(collectionsMap));
+        // }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
 }
 export function* fetchCollectionsStart() {
-    yield takeEvery(
+    yield takeLatest(
         ShopActionTypes.FETCH_COLLECTIONS_START, 
         fetchCollectionsAsync
     );
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,7 @@ import logger from 'redux-logger';
 
 import createSagaMiddleware from 'redux-saga';
 
-import { fetchCollectionsStart } from './shop/shop.sagas';
-
+import rootSaga from './root.saga';
 import rootReducer from './root-reducer';
 
 const sagaMiddleware = createSagaMiddleware();
@@ -18,7 +17,7 @@ if(process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 // export default { store, persistStore };
